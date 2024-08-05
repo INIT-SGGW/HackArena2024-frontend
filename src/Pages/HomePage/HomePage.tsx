c7217ee9b4171b6ce5eae325d4e0e2cb388d16d1
VEXI19 <87818072+VEXI19@users.noreply.github.com>
2024-08-05 13:03:55 +0200

    updated front page design

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -4,7 +4,7 @@ import useTimeToEvent from "../../Hooks/useTimeToEvent";
 import { getEventDate, getEventTime, getRegistrationEndDate } from "../../Constants/Constants";
 import text from "../../Assets/text.json";
 import ChevronIcon from "../../Assets/chevron-down.svg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import isRegistrationOpen from "../../Utils/isRegistrationOpen";
 import isEventLive from "../../Utils/isEventLive";
 import isEventDone from "../../Utils/isEventDone";
@@ -39,21 +39,34 @@ function HomePage(props: Props) {
   const navigate = useNavigate();
   const timeToEvent = useTimeToEvent();
   const homeText = text.home;
-  const span: string = "<span>asdf</span>"
+
+  useEffect(() => {
+    const aboutText = document.getElementById("about_text");
+    const nextEventText = document.getElementById("next_event_text");
+
+    if (aboutText) {
+      aboutText.innerHTML = homeText.aboutUs.description;
+    }
+
+    if (nextEventText) {
+      nextEventText.innerHTML = homeText.nextEvent.description;
+    }
+
+  }, []);
+
   return (
     <div className="home">
-      <div id="welcome" className="home--welcome pagewidth home--section">
-        <h1>{homeText.welcome.title}</h1>
-        <div className="welcome--text">
-          <h6>{homeText.welcome.description.first} <span>{homeText.welcome.description.second}</span></h6>
-          <p>{homeText.welcome.slogan}</p>
-        </div>
-        <div className="welcome--buttons">
-          <button onClick={() => navigate("/rejestracja")} className="account--button account--button__primary">{homeText.welcome.buttons.register}</button>
-          <button onClick={() => window.location.href = "#zadanie"} className="account--button account--button__secondary">{homeText.welcome.buttons.seeMore}</button>
+      <div id="welcome--wrapper">
+        <div id="welcome" className="home--welcome pagewidth home--section">
+          <h1>{homeText.welcome.title}</h1>
+          <h6>{homeText.welcome.description.first}</h6>
+          <div className="welcome--buttons">
+            <button onClick={() => navigate("/wydarzenia")} className="account--button account--button__primary">{homeText.welcome.buttons.events}</button>
+            <button onClick={() => window.location.href = "#o_nas"} className="account--button account--button__secondary">{homeText.welcome.buttons.aboutUs}</button>
+          </div>
         </div>
       </div>
-      <div className="home--date home--section">
+      {/* <div className="home--date home--section">
         {
           isEventLive() ?
             <>
@@ -69,12 +82,19 @@ function HomePage(props: Props) {
                 <h1>{timeToEvent}</h1>
               </>
         }
+      </div> */}
+      <div id="o_nas" className="home--about pagewidth home--section">
+        <h2>{homeText.aboutUs.title}</h2>
+        <p id="about_text"></p>
       </div>
-      <div id="zadanie" className="home--about pagewidth home--section">
-        <h1>{homeText.zadanie.title}</h1>
-        <p>{homeText.zadanie.description.first} <span>{homeText.zadanie.description.second}</span> {homeText.zadanie.description.third} <span>{homeText.zadanie.description.fourth}</span> {homeText.zadanie.description.fifth} <span>{homeText.zadanie.description.sixth}</span> {homeText.zadanie.description.seventh} <span>{homeText.zadanie.description.eighth}</span></p>
+      <div className="home--about__event">
+        <div id="hackarena2" className="home--about pagewidth home--section">
+          <h2>{homeText.nextEvent.title}</h2>
+          <p id="next_event_text"></p>
+          <button onClick={() => navigate("/wydarzenia/hackarena2")} className="account--button account--button__primary-secondary">{homeText.nextEvent.button}</button>
+        </div>
       </div>
-      <div id="agenda" className="home--agenda home--section pagewidth">
+      {/* <div id="agenda" className="home--agenda home--section pagewidth">
         <h1>{homeText.agenda.title}</h1>
         <div className="agenda--schedule">
           {
@@ -87,17 +107,17 @@ function HomePage(props: Props) {
             })
           }
         </div>
-      </div>
+      </div> */}
       {
         isRegistrationOpen() &&
         <div id='nieczekaj' className="home--dwait pagewidth home--section">
-          <h1>{homeText.dontWait.title}</h1>
+          <h2>{homeText.dontWait.title}</h2>
           <h4>{homeText.dontWait.description}</h4>
           <p>{homeText.dontWait.dateReminder} <b>{getRegistrationEndDate()}</b></p>
           <button onClick={() => navigate("/rejestracja")} className="account--button account--button__primary">{homeText.dontWait.button}</button>
         </div>}
       <div id="faq" className="home--faq pagewidth home--section">
-        <h1>{homeText.faq.title}</h1>
+        <h2>{homeText.faq.title}</h2>
         <div className="faq--content">
           {
             homeText.faq.questions.map(({ question, answer }, index) => {
