5d3d84f3b343c03e956e8d14d00cb6f21d3c6ec5
VEXI19 <87818072+VEXI19@users.noreply.github.com>
2024-08-20 17:48:39 +0200

    added toDo to notFound

--- src/Pages/NotFound/NotFound.tsx
+++ src/Pages/NotFound/NotFound.tsx
@@ -4,6 +4,7 @@ import "./NotFound.css";
 interface Props { }
 
 function NotFound(props: Props) {
+  //TODO: new styling
   return <div className="notfound pagewidth">
     <h4>Błąd 404</h4>
     <h6>Strona, którą próbujesz otworzyć nie istnieje</h6>
