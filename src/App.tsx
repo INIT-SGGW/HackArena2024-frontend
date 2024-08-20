93dc50ce8637703fd2f9c87313d11d52a2312714
VEXI19 <87818072+VEXI19@users.noreply.github.com>
2024-08-20 17:47:57 +0200

    moved topbar and sidebar components inside app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,64 @@
-import { Outlet } from "react-router";
-import NavBar from "./Components/NavBar/NavBar";
-import Footer from "./Components/Footer/Footer";
 import "./App.css";
-import { ScrollRestoration, useNavigate } from "react-router-dom";
-import text from "./Assets/text.json";
+
+import { Outlet, Link, ScrollRestoration } from "react-router-dom";
+import { useState } from "react";
+
+// Components
+import NavBar from "./Components/NavBar/NavBar";
+
+// Assets
+import Logo from "./Assets/logo.svg"
+import FacebookIcon from "./Assets/facebook.svg";
+import InstagramIcon from "./Assets/instagram.svg";
+import DiscordIcon from "./Assets/discord.svg";
+import LinkedInIcon from "./Assets/linkedin.svg";
+
+
+const TopBar = () => {
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  return (
+    <div className="topbar__wrapper">
+      <div className="topbar pagewidth">
+        <Link to="/">
+          <img src={Logo} alt="HackArena" />
+        </Link>
+        <NavBar showSideBar={showSidebar} setShowSidebar={setShowSidebar} />
+
+      </div>
+    </div>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <NavBar />
+      <div className="divider" />
+      <div className="footer--social">
+        <a href="https://www.facebook.com/profile.php?id=61559358943109&is_tour_dismissed">
+          <img src={FacebookIcon} alt="Facebook" />
+        </a>
+        <a href="https://www.instagram.com/_init_2024/">
+          <img src={InstagramIcon} alt="Instagram" />
+        </a>
+        <a href="https://discord.com/invite/YekgmBp9K4">
+          <img src={DiscordIcon} alt="Discord" />
+        </a>
+        <a href="https://www.linkedin.com/company/ko%C5%82o-naukowe-init/about/">
+          <img src={LinkedInIcon} alt="LinkedIn" />
+        </a>
+
+      </div>
+    </div>
+  )
+}
 
 function App() {
-  const navigate = useNavigate();
-
   return (
     <div className="app">
       <ScrollRestoration />
-      {/* <div className="topbar" onClick={() => navigate("/wydarzenia/hackarena2_0")}>
-        <p>{text.topBar.text}</p>
-      </div> */}
-      <NavBar />
+      <TopBar />
       <div className="app--content">
         <Outlet />
       </div>
