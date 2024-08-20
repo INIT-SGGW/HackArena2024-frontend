59e0d58fb51fbec03d9a222f61756119935aabfc
VEXI19 <87818072+VEXI19@users.noreply.github.com>
2024-08-20 17:49:09 +0200

    changed navBar to be universal across the website

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -2,17 +2,18 @@ import { useState } from "react";
 import "./NavBar.css";
 import text from "../../Assets/text.json";
 import { Link } from "react-router-dom";
-import Logo from "../../Assets/logo.svg";
 import useWindowWidth from "../../Hooks/useWindowWidth";
 import Menu from "../../Assets/menu.svg";
 // import MenuOpen from "../../Assets/menu-open.svg";
 
-export const NavBarButton = ({ text, link, special = false }: { text: string, link: string, special?: boolean }) => {
-  console.log(text, special)
+export const NavBarButton = ({ text, link, setShowSidebar, special = false }: { text: string, link: string, setShowSidebar?: Function, special?: boolean }) => {
   return (
     <div className="navbarButton">
       <Link
         to={link}
+        onClick={() => {
+          if (setShowSidebar) setShowSidebar(false);
+        }}
         className={`navbar__link${special === true ? " navbar__link--special" : ""}`}
       >
         {text}
@@ -29,87 +30,42 @@ export const NavBarButton = ({ text, link, special = false }: { text: string, li
   )
 }
 
-function NavBar() {
-  const [showSidebar, setShowSidebar] = useState(false);
+function NavBar({ showSideBar, setShowSidebar }: { showSideBar?: boolean, setShowSidebar?: Function }) {
   const windowWidth = useWindowWidth();
   const navItems = text.nav.navItems;
+
+  const NavSection = () => (
+    <nav className="navbar__nav">
+      {navItems.map((item, index) => {
+        return (
+          <NavBarButton key={index} text={item.title} link={item.path} setShowSidebar={setShowSidebar} special={item.special} />
+        );
+      })}
+    </nav>
+  )
+
   return (
     <div className="navbar">
-      <div className="navbar__content pagewidth">
-        <Link to="/" onClick={() => setShowSidebar(false)}>
-          <img src={Logo} alt="HackArena" />
-        </Link>
-        {windowWidth < 768 ? (
-          <>
-            <a
-              className={`navbar__openside${!showSidebar ? " navbar__openside--closed" : ""
-                }`}
-              onClick={() => setShowSidebar(!showSidebar)}
-            >
-              <img src={Menu} alt="-" />
-              {/* <img src={MenuOpen} alt="-" /> */}
-            </a>
-            <div
-              className={`navbar__sidebar${!showSidebar ? " navbar__sidebar--hidden" : ""
-                }`}
-            >
-              <nav className="navbar__nav">
-                <Link
-                  to="/rejestracja"
-                  onClick={() => setShowSidebar(false)}
-                  className="navbar__link navbar__link--special"
-                >
-                  {navItems[0]}
-                </Link>
-                <Link
-                  to="/wydarzenia"
-                  onClick={() => setShowSidebar(false)}
-                  className="navbar__link"
-                >
-                  {navItems[1]}
-                </Link>
-                {navItems.slice(2, navItems.length).map((item, index) => {
-                  return (
-                    <a
-                      key={index}
-                      href={`/#${item.toLowerCase()}`}
-                      className="navbar__link"
-                      onClick={() => setShowSidebar(false)}
-                    >
-                      {item}
-                    </a>
-                  );
-                })}
-                {/* <Link
-                  to="/login"
-                  onClick={() => setShowSidebar(false)}
-                  className="navbar__link"
-                >
-                  {navItems[navItems.length - 1]}
-                </Link> */}
-              </nav>
-            </div>
-          </>
-        ) : (
-          <>
-            <nav className="navbar__nav">
-              <NavBarButton text={navItems[0]} link="/rejestracja" special={true} />
-              <NavBarButton text={navItems[1]} link="/wydarzenia" />
 
-              {navItems.slice(2, navItems.length).map((item, index) => {
-                return (
-                  <NavBarButton key={index} text={item} link={`/#${item.toLowerCase()}`} />
-
-                );
-              })}
-              {/* <Link to="/login" className="navbar__link">
-                {navItems[navItems.length - 1]}
-              </Link> */}
-            </nav>
-          </>
-        )
-        }
-      </div >
+      {(windowWidth < 768 && setShowSidebar !== undefined && showSideBar !== undefined) ? (
+        <>
+          <a
+            className="navbar__sidebar-icon"
+            onClick={() => setShowSidebar(!showSideBar)}
+          >
+            <img src={Menu} alt="Menu" />
+          </a>
+          <div
+            className={`navbar__sidebar${!showSideBar ? " navbar__sidebar--hidden" : ""
+              }`}
+          >
+            <NavSection />
+          </div>
+        </>
+      ) : (
+        <NavSection />
+      )
+      }
     </div >
   );
 }
