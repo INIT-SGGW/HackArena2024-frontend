dcce194cdd5c3146b6a813073b2a1d19d78ba5e5
VEXI19 <87818072+VEXI19@users.noreply.github.com>
2024-08-24 20:01:07 +0200

    refactor: Update NotFound page with new styling and navigation buttons

--- src/Pages/NotFound/NotFound.tsx
+++ src/Pages/NotFound/NotFound.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import "./NotFound.css";
+import { useNavigate } from "react-router";
+import text from "../../Assets/text.json";
+import Button from "../../Components/Button/Button";
 
 interface Props { }
 
 function NotFound(props: Props) {
-  //TODO: new styling
-  return <div className="notfound pagewidth">
-    <h4>Błąd 404</h4>
-    <h6>Strona, którą próbujesz otworzyć nie istnieje</h6>
-  </div>;
+  const pageText = text.notFound;
+  const navigation = useNavigate();
+
+  return (
+    <div className="notfound pagewidth">
+      <h2 className="header__white">{pageText.title}</h2>
+      <h6>{pageText.description}</h6>
+      <div>
+        <Button className="btn btn__primary-w" onClick={() => navigation("/")}>{pageText.buttons.home}</Button>
+        <Button className="btn btn__secondary-w" onClick={() => navigation(-1)}>{pageText.buttons.goBack}</Button>
+      </div>
+    </div>
+  )
 }
 
 export default NotFound;
