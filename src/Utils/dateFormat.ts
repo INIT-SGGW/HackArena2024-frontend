16d6de736df132c55b271aac54f9b400e907222a
VEXI19 <87818072+VEXI19@users.noreply.github.com>
2024-08-28 14:25:24 +0200

    refactor: Remove unused console.log statements in dateFormat.ts

--- src/Utils/dateFormat.ts
+++ src/Utils/dateFormat.ts
@@ -7,15 +7,10 @@ export enum DateFormat {
 const dateFormat = (date: Date, format: DateFormat = DateFormat.FULL): string => {
     switch (format) {
         case DateFormat.DATE:
-            console.log("date")
             return date.toLocaleDateString("pl-PL");
         case DateFormat.TIME:
-            console.log("time")
-
             return date.toLocaleTimeString("pl-PL");
         case DateFormat.FULL:
-            console.log("full")
-
             return date.toLocaleString("pl-PL");
     }
 }
